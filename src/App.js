1619462bb2cb7eb533fa885bf35bca8bed27c367
Patrick Nepp <ptnepp@gmail.com>
2023-01-18 13:43:11 -0600

    categories component/styling and data file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Category from "./components/category/category.component";
+import { categories } from "./data/categories";
+import './categories.styles.scss'
 
 const App = () => {
+
   return (
-    <div>
-      Hello World!  
+    <div className="categories-container">
+      {categories.map((cat) => {
+        return <Category key={cat.id} category={cat} />
+      })}
     </div>
   );
 }
