220080eafc6547e18a812e903f037b16ac2c8c90
Patrick Nepp <ptnepp@gmail.com>
2023-01-21 11:46:50 -0600

    sign in with redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,20 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
+import { useEffect } from "react"
+import { getRedirectResult } from "firebase/auth"
+
+import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect } from "../../utils/firebase/firebase.utils"
 
 const SignIn = () => {
+  useEffect(() => {
+    const redirectResult = async () => {
+      const response = await getRedirectResult(auth)
+      if(response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user)
+      }
+    }
+
+    redirectResult()
+  }, [])
+
   const logGoogleUser = async () => {
     const {user} = await signInWithGooglePopup()
     const userDocRef = await createUserDocumentFromAuth(user)
@@ -11,6 +25,9 @@ const SignIn = () => {
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser} >
         Sign in with Google Popup
+      </button>
+      <button onClick={signInWithGoogleRedirect} >
+        Sign in with Google Redirect
       </button>
     </div>
   )
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -23,6 +23,7 @@ provider.setCustomParameters({
 
 export const auth = getAuth()
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+export const signInWithGoogleRedirect = () => signInWithGoogleRedirect(auth, provider)
 
 export const db = getFirestore()
 
