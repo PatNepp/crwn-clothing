3f2343bf614dc7f55ae37e3fd7cbd8f4a4304e21
Patrick Nepp <ptnepp@gmail.com>
2023-01-25 20:44:08 -0600

    add collection to firebase and get category docs from firebase in category map

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,14 +1,23 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-import PRODUCTS from '../data/shop-data.json'
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const ProductsContext = createContext({
     products: []
 })
 
 export const ProductsProvider = ({children}) => {
-    const [products, setProducts] = useState(PRODUCTS)
+    const [products, setProducts] = useState([])
     const value = {products}
+
+    useEffect(() => {
+        const getCategoryMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments()
+            console.log(categoryMap)
+        }
+
+        getCategoryMap()
+    }, [])
 
     return <ProductsContext.Provider value={value} >{children}</ProductsContext.Provider>
 }
